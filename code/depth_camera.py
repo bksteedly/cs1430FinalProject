ca75be626634c5fbeaae0e4f788c1311641d5794
Amulya Velamakanni <avelamak@terpmail.umd.edu>
2025-04-28 14:23:04 -0400

    point cloud

--- code/depth_camera.py
+++ code/depth_camera.py
@@ -277,98 +277,78 @@ def point_cloud():
 
     out = np.empty((h, w, 3), dtype=np.uint8)
 
-    while True:
-        # Grab camera data
-        if not state.paused:
-            # Wait for a coherent pair of frames: depth and color
-            frames = pipeline.wait_for_frames()
+    try:
+        while True:
+            print('inside while loop')
+            print(f'state: {state.paused}')
+            # Grab camera data
+            if not state.paused:
+                # Wait for a coherent pair of frames: depth and color
+                frames = pipeline.wait_for_frames()
 
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
+                depth_frame = frames.get_depth_frame()
+                color_frame = frames.get_color_frame()
 
-            depth_frame = decimate.process(depth_frame)
+                depth_frame = decimate.process(depth_frame)
 
-            # Grab new intrinsics (may be changed by decimation)
-            depth_intrinsics = rs.video_stream_profile(
-                depth_frame.profile).get_intrinsics()
-            w, h = depth_intrinsics.width, depth_intrinsics.height
+                # Grab new intrinsics (may be changed by decimation)
+                depth_intrinsics = rs.video_stream_profile(
+                    depth_frame.profile).get_intrinsics()
+                w, h = depth_intrinsics.width, depth_intrinsics.height
 
-            depth_image = np.asanyarray(depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+                depth_image = np.asanyarray(depth_frame.get_data())
+                color_image = np.asanyarray(color_frame.get_data())
 
-            depth_colormap = np.asanyarray(
-                colorizer.colorize(depth_frame).get_data())
+                depth_colormap = np.asanyarray(
+                    colorizer.colorize(depth_frame).get_data())
 
-            if state.color:
-                mapped_frame, color_source = color_frame, color_image
+                if state.color:
+                    mapped_frame, color_source = color_frame, color_image
+                else:
+                    mapped_frame, color_source = depth_frame, depth_colormap
+
+                points = pc.calculate(depth_frame)
+                pc.map_to(mapped_frame)
+
+                # Pointcloud data to arrays
+                v, t = points.get_vertices(), points.get_texture_coordinates()
+                verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+                texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+
+            # Render
+            now = time.time()
+
+            out.fill(0)
+
+            grid(out, (0, 0.5, 1), size=1, n=10)
+            frustum(out, depth_intrinsics)
+            axes(out, view([0, 0, 0]), state.rotation, size=0.1, thickness=1)
+
+            if not state.scale or out.shape[:2] == (h, w):
+                pointcloud(out, verts, texcoords, color_source)
             else:
-                mapped_frame, color_source = depth_frame, depth_colormap
+                tmp = np.zeros((h, w, 3), dtype=np.uint8)
+                pointcloud(tmp, verts, texcoords, color_source)
+                tmp = cv2.resize(
+                    tmp, out.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+                np.putmask(out, tmp > 0, tmp)
 
-            points = pc.calculate(depth_frame)
-            pc.map_to(mapped_frame)
+            if any(state.mouse_btns):
+                axes(out, view(state.pivot), state.rotation, thickness=4)
 
-            # Pointcloud data to arrays
-            v, t = points.get_vertices(), points.get_texture_coordinates()
-            verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
-            texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+            dt = time.time() - now
 
-        # Render
-        now = time.time()
+            cv2.setWindowTitle(
+                state.WIN_NAME, "RealSense (%dx%d) %dFPS (%.2fms) %s" %
+                (w, h, 1.0/dt, dt*1000, "PAUSED" if state.paused else ""))
 
-        out.fill(0)
+            cv2.imshow(state.WIN_NAME, out)
+            cv2.waitKey(1)
 
-        grid(out, (0, 0.5, 1), size=1, n=10)
-        frustum(out, depth_intrinsics)
-        axes(out, view([0, 0, 0]), state.rotation, size=0.1, thickness=1)
-
-        if not state.scale or out.shape[:2] == (h, w):
-            pointcloud(out, verts, texcoords, color_source)
-        else:
-            tmp = np.zeros((h, w, 3), dtype=np.uint8)
-            pointcloud(tmp, verts, texcoords, color_source)
-            tmp = cv2.resize(
-                tmp, out.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
-            np.putmask(out, tmp > 0, tmp)
-
-        if any(state.mouse_btns):
-            axes(out, view(state.pivot), state.rotation, thickness=4)
-
-        dt = time.time() - now
-
-        cv2.setWindowTitle(
-            state.WIN_NAME, "RealSense (%dx%d) %dFPS (%.2fms) %s" %
-            (w, h, 1.0/dt, dt*1000, "PAUSED" if state.paused else ""))
-
-        cv2.imshow(state.WIN_NAME, out)
-        key = cv2.waitKey(1)
-
-        if key == ord("r"):
-            state.reset()
-
-        if key == ord("p"):
-            state.paused ^= True
-
-        if key == ord("d"):
-            state.decimate = (state.decimate + 1) % 3
-            decimate.set_option(rs.option.filter_magnitude, 2 ** state.decimate)
-
-        if key == ord("z"):
-            state.scale ^= True
-
-        if key == ord("c"):
-            state.color ^= True
-
-        if key == ord("s"):
             cv2.imwrite('./out.png', out)
-
-        if key == ord("e"):
-            points.export_to_ply('./out.ply', mapped_frame)
-
-        if key in (27, ord("q")) or cv2.getWindowProperty(state.WIN_NAME, cv2.WND_PROP_AUTOSIZE) < 0:
-            break
-
-    # Stop streaming
-    pipeline.stop()
+    finally:
+        # Stop streaming
+        pipeline.stop()
 
 
 def stream():
@@ -471,4 +451,4 @@ def test():
 
 
 if __name__ == '__main__':
-    stream()
+    point_cloud()
